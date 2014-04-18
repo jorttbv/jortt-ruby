a518a38bc215ec9a3db27e45675922ee0face294
Steven Weller <github@suweller.com>
2014-04-18 12:06:34 +0200

    Add Freemle.client convenience method
    Version bump to 1.0.1
    
    * added Freemle.client
    * moved Freemle::Resource -> Freemle::Client::Resource

--- lib/freemle.rb
+++ lib/freemle.rb
@@ -3,4 +3,14 @@ require 'freemle/client/version'
 
 module Freemle
 
+  # Convenient way to initialize a freemle client.
+  #
+  # @see {Freemle::Client.initialize}
+  #
+  # @since 1.0.1
+  def client(*args)
+    Freemle::Client.new(*args)
+  end
+  module_function :client
+
 end
