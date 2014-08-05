d846d209a66be0f031c05bf8586f126797f1b954
Steven Weller <github@suweller.com>
2014-08-06 00:06:33 +0200

    Fix typo

--- spec/freemle_spec.rb
+++ spec/freemle_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Freemle do
   describe '.client' do
-    subject { descibed_class.client(app_name: 'name', api_key: 'secret') }
+    subject { described_class.client(app_name: 'name', api_key: 'secret') }
     it { should be_instance_of(described_class::Client) }
   end
 end
