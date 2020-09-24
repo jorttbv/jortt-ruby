a34c60d0f86dd95a8c2ae54b303dc1a91e5ce811
Stephan van Diepen <stephan@vandiepen.info>
2020-09-24 11:11:30 +0200

    Remove old skool hash rocket

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ VCR.configure do |c|
   c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.default_cassette_options = { :record => :once }
+  c.default_cassette_options = { record: :once }
 
   c.before_record do |i|
     i.response.headers.delete('Set-Cookie')
